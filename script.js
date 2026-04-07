9903fd3338375cd01858066c8586b844bc72a9bd
tmallma1-ui <tmallma1@upshurschools.com>
2026-04-07 08:44:26 -0400

    Added API data

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hou
   .catch(error => { 
     console.log("Error:", error); 
   });
+fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m")
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("output").innerHTML =
+      "Latitude: " + data.latitude + "<br>" +
+      "Longitude: " + data.longitude + "<br>" +
+      "First temperature: " + data.hourly.temperature_2m[0];
+  });
